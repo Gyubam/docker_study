cf46ef58562c77c048f79cf20b99c513be07341e
sgb8170@naver.com <sgb8170@naver.com>
2022-09-26 00:03:39 +0900

    Utility container setting

--- multi-01-starting-setup/backend/app.js
+++ multi-01-starting-setup/backend/app.js
@@ -84,8 +84,9 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://mongodb:27017/course-goals',
-  // `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?AutoSource=admin`,
+  // 'mongodb://mongodb:27017/course-goals',
+  'mongodb://max:secret@mongodb:27017/course-goals?authSource=admin',
+  // `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin&authMechanism=SCRAM-SHA-1`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
